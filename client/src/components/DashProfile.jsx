50689a5d7f66f5b41514f9680a908a79df81a037
star135 <afshinstar2000@gmail.com>
2024-09-28 13:45:19 +0330

    complete delete user account functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -208,9 +208,10 @@ export default function DashProfile() {
                 {updateUserError}
 
             </Alert>
-        {error && (
+        )}
+        {updateUserError && (
             <Alert color='failure' className='mt-5'>
-                {error}
+                {updateUserError}
 
             </Alert>
         )}
