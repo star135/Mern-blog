81a7dbbad68506470f5687d9ba88ea46077b3b7a
star135 <afshinstar2000@gmail.com>
2024-09-29 10:56:58 +0330

    Complete create a post page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 export default function App() {
   return (
@@ -20,6 +22,11 @@ export default function App() {
         <Route element={<PrivateRoute/>}>
           <Route path='/dashboard' element={<Dashboard/>}/>
         </Route>
+
+        <Route element={<OnlyAdminPrivateRoute/>}>
+          <Route path='/create-post' element={<CreatePost/>}/>
+        </Route>
+
         <Route path='/sign-in' element={<Signin/>}/>
         <Route path='/sign-up' element={<Signup/>}/>
         <Route path='/projects' element={<Projects/>}/>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -10,9 +10,10 @@ import { updateStart, updateSuccess, updateFailure,
  } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import {HiOutlineExclamationCircle} from 'react-icons/hi';
+import {Link} from 'react-router-dom';
 
 export default function DashProfile() {
-    const {currentUser, error} = useSelector(state =>state.user);
+    const {currentUser, error, loading} = useSelector(state =>state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -207,9 +208,18 @@ export default function DashProfile() {
             <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}  onChange={handleChange} />
             <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
             <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-            <Button type='submit' gradientDuoTone="purpleToBlue" outline>
-                Update
+            <Button type='submit' gradientDuoTone="purpleToBlue" outline disabled={loading || imageFileUploading}>
+                {loading ? 'Loading...' : 'Update'}
             </Button>
+
+            {currentUser.isAdmin && (
+                <Link to={'/create-post'}>
+                <Button
+                type='button' gradientDuoTone='purpleToPink' className='w-full'>
+                    Create a Post
+                </Button>
+                </Link>
+            )}
         </form>
         <div className='text-red-700 flex justify-between mt-5'>
             <span onClick={()=>setShowModal(true)} className='cursor-pointer'>Delete Account</span>
